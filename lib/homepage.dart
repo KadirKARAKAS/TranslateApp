b8a3d1345e17b42b3db6bba3a0f77f3de0bae2e5
Kadir KARAKAS <kadirkarakas5546@gmail.com>
2024-05-24 19:40:23 +0300

    save

---
+++ lib/homepage.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("HOME PAGE"),
+      ),
+      body: Center(child: TextToSpeech()),
+    );
+  }
+}
+
+class TextToSpeech extends StatelessWidget {
+  final FlutterTts flutterTts = FlutterTts();
+  final TextEditingController textEditingController = TextEditingController();
+  speak(String text) async {
+    await flutterTts.setLanguage("en-US");
+    await flutterTts.setPitch(1); //0.5-1.5
+    await flutterTts.speak(text);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: Padding(
+        padding: EdgeInsets.all(32),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            TextFormField(
+              controller: textEditingController,
+            ),
+            ElevatedButton(
+              child: Text("SESLİ OKU"),
+              onPressed: () => speak(textEditingController.text),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
