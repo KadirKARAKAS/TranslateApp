6195f109feaa1dc2cf1e833fc352a8b8515e08dd
Kadir KARAKAS <kadirkarakas5546@gmail.com>
2024-05-26 19:42:54 +0300

    savetranslate

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -29,41 +29,38 @@ class TextToSpeechWithWaveform extends StatefulWidget {
       _TextToSpeechWithWaveformState();
 }
 
-// Text-to-speech ve dalgalanma animasyonunun durumu
 class _TextToSpeechWithWaveformState extends State<TextToSpeechWithWaveform> {
-  final FlutterTts flutterTts = FlutterTts(); // Text-to-speech nesnesi
-  final TextEditingController textEditingController =
-      TextEditingController(); // Metin girişi kontrolcüsü
-  bool isSpeaking = false; // Konuşma durumu
+  final FlutterTts flutterTts = FlutterTts();
+  final TextEditingController textEditingController = TextEditingController();
+  bool isSpeaking = false;
 
   @override
   void initState() {
     super.initState();
     flutterTts.setCompletionHandler(() {
       setState(() {
-        isSpeaking = false; // Konuşma bittiğinde durumu güncelle
+        isSpeaking = false;
       });
     });
     flutterTts.setErrorHandler((msg) {
       setState(() {
-        isSpeaking = false; // Hata oluştuğunda durumu güncelle
+        isSpeaking = false;
       });
     });
   }
 
-  // Metni sesli okuma fonksiyonu
   Future<void> speak(String text) async {
     setState(() {
-      isSpeaking = true; // Konuşma başladığında durumu güncelle
+      isSpeaking = true;
     });
-    await flutterTts.setLanguage("tr-TR"); // Dil ayarı
-    await flutterTts.setPitch(1); // Ses tonu ayarı (0.5-1.5 arası)
-    await flutterTts.speak(text); // Metni oku
+    await flutterTts.setLanguage("tr-TR");
+    await flutterTts.setPitch(1);
+    await flutterTts.speak(text);
   }
 
   @override
   void dispose() {
-    textEditingController.dispose(); // Kaynakları serbest bırak
+    textEditingController.dispose();
     super.dispose();
   }
 
@@ -72,36 +69,44 @@ class _TextToSpeechWithWaveformState extends State<TextToSpeechWithWaveform> {
     return Container(
       alignment: Alignment.center,
       child: Padding(
-        padding: const EdgeInsets.all(32), // Kenar boşlukları
+        padding: const EdgeInsets.all(32),
         child: Stack(
           alignment: Alignment.center,
           children: <Widget>[
-            if (isSpeaking) // Konuşma durumuna göre dalgalanma animasyonu göster
+            if (isSpeaking)
               const Positioned(
                 top: 0,
                 child: SoundWaveformWidget(
-                  count: 10, // Çubuk sayısı
-                  minHeight: 20, // Çubukların minimum yüksekliği
-                  maxHeight: 100, // Çubukların maksimum yüksekliği
+                  count: 10,
+                  minHeight: 20,
+                  maxHeight: 100,
+                  isAnimating: true,
+                ),
+              )
+            else
+              const Positioned(
+                top: 0,
+                child: SoundWaveformWidget(
+                  count: 10,
+                  minHeight: 20,
+                  maxHeight: 100,
+                  isAnimating: false,
                 ),
               ),
             Column(
               mainAxisSize: MainAxisSize.min,
               children: <Widget>[
-                const SizedBox(
-                    height: 120), // TextFormField'ı aşağı taşımak için boşluk
+                const SizedBox(height: 120),
                 TextFormField(
-                  controller: textEditingController, // Metin giriş kontrolcüsü
+                  controller: textEditingController,
                   decoration: const InputDecoration(
-                    labelText: "Enter text", // Giriş kutusu etiketi
+                    labelText: "Enter text",
                   ),
                 ),
-                const SizedBox(
-                    height: 20), // Giriş kutusu ile buton arasındaki boşluk
+                const SizedBox(height: 20),
                 ElevatedButton(
-                  child: const Text("SESLİ OKU"), // Buton metni
-                  onPressed: () => speak(
-                      textEditingController.text), // Butona basılınca metni oku
+                  child: const Text("SESLİ OKU"),
+                  onPressed: () => speak(textEditingController.text),
                 ),
               ],
             ),
@@ -112,61 +117,61 @@ class _TextToSpeechWithWaveformState extends State<TextToSpeechWithWaveform> {
   }
 }
 
-// Dalgalanma animasyonu widget'ı
 class SoundWaveformWidget extends StatefulWidget {
-  final int count; // Çubuk sayısı
-  final double minHeight; // Çubukların minimum yüksekliği
-  final double maxHeight; // Çubukların maksimum yüksekliği
-  final int durationInMilliseconds; // Animasyon süresi
+  final int count;
+  final double minHeight;
+  final double maxHeight;
+  final bool isAnimating;
 
   const SoundWaveformWidget({
     Key? key,
-    this.count = 10, // Artırılmış çubuk sayısı
-    this.minHeight = 20, // Artırılmış minimum yükseklik
-    this.maxHeight = 100, // Artırılmış maksimum yükseklik
-    this.durationInMilliseconds = 800, // Animasyon süresi
+    this.count = 10,
+    this.minHeight = 20,
+    this.maxHeight = 100,
+    this.isAnimating = false,
   }) : super(key: key);
 
   @override
   State<SoundWaveformWidget> createState() => _SoundWaveformWidgetState();
 }
 
-// Dalgalanma animasyonunun durumu
 class _SoundWaveformWidgetState extends State<SoundWaveformWidget>
     with TickerProviderStateMixin {
-  late AnimationController controller; // Animasyon kontrolcüsü
-  late List<double> heights; // Çubukların yükseklik listesi
-  final Random random = Random(); // Rastgele sayı üretici
+  late AnimationController controller;
+  late List<double> heights;
+  final Random random = Random();
 
   @override
   void initState() {
     super.initState();
     controller = AnimationController(
-      vsync:
-          this, // TickerProviderStateMixin'den gelen animasyon senkronizasyonu
-      duration: Duration(
-          milliseconds: widget.durationInMilliseconds), // Animasyon süresi
-    )..repeat(); // Animasyonu tekrar et
-    heights = List<double>.filled(
-        widget.count, widget.minHeight); // Çubuk yüksekliklerini başlat
-    _randomizeHeights(); // Çubuk yüksekliklerini rastgele ayarla
+      vsync: this,
+      duration: Duration(milliseconds: widget.isAnimating ? 800 : 0),
+    );
+
+    if (widget.isAnimating) {
+      controller.repeat();
+    }
+
+    heights = List<double>.filled(widget.count, widget.minHeight);
+
+    if (widget.isAnimating) {
+      _randomizeHeights();
+    }
   }
 
-  // Çubuk yüksekliklerini rastgele ayarlayan fonksiyon
   void _randomizeHeights() {
     Future.delayed(
-      Duration(milliseconds: widget.durationInMilliseconds ~/ widget.count),
+      Duration(milliseconds: 800 ~/ widget.count),
       () {
-        if (mounted) {
+        if (mounted && widget.isAnimating) {
           setState(() {
             for (int i = 0; i < heights.length; i++) {
               heights[i] = widget.minHeight +
-                  random.nextDouble() *
-                      (widget.maxHeight -
-                          widget.minHeight); // Rastgele yükseklik ayarla
+                  random.nextDouble() * (widget.maxHeight - widget.minHeight);
             }
           });
-          _randomizeHeights(); // Yeniden çağırarak sürekli güncelle
+          _randomizeHeights();
         }
       },
     );
@@ -174,36 +179,52 @@ class _SoundWaveformWidgetState extends State<SoundWaveformWidget>
 
   @override
   void dispose() {
-    controller.dispose(); // Kaynakları serbest bırak
+    controller.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: controller, // Animasyon kontrolcüsüne bağlı
-      builder: (context, child) {
-        return Row(
-          mainAxisSize: MainAxisSize.min,
-          children: List.generate(
-            widget.count, // Çubuk sayısına göre çubuk oluştur
-            (i) => AnimatedContainer(
-              duration: Duration(
-                  milliseconds: widget.durationInMilliseconds ~/ widget.count),
-              margin: i == (widget.count - 1)
-                  ? EdgeInsets.zero
-                  : const EdgeInsets.only(right: 5), // Çubuklar arasına boşluk
-              height: heights[i], // Çubuğun yüksekliği
-              width: 20, // Çubuğun genişliği
-              decoration: BoxDecoration(
-                color: Colors.black, // Çubuğun rengi
-                borderRadius:
-                    BorderRadius.circular(9999), // Çubuğun köşe yuvarlaklığı
+    return widget.isAnimating
+        ? AnimatedBuilder(
+            animation: controller,
+            builder: (context, child) {
+              return Row(
+                mainAxisSize: MainAxisSize.min,
+                children: List.generate(
+                  widget.count,
+                  (i) => AnimatedContainer(
+                    duration: Duration(milliseconds: 800 ~/ widget.count),
+                    margin: i == (widget.count - 1)
+                        ? EdgeInsets.zero
+                        : const EdgeInsets.only(right: 5),
+                    height: heights[i],
+                    width: 20,
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(9999),
+                    ),
+                  ),
+                ),
+              );
+            },
+          )
+        : Row(
+            mainAxisSize: MainAxisSize.min,
+            children: List.generate(
+              widget.count,
+              (i) => Container(
+                margin: i == (widget.count - 1)
+                    ? EdgeInsets.zero
+                    : const EdgeInsets.only(right: 5),
+                height: widget.minHeight,
+                width: 20,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(9999),
+                ),
               ),
             ),
-          ),
-        );
-      },
-    );
+          );
   }
 }
